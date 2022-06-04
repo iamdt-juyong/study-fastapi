d049f68981bcae102ef545dd18336eee0b3b9a2b
Juyong <106858112+iamdt-juyong@users.noreply.github.com>
2022-06-04 22:39:38 +0900

    temp

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query, Body
 from enum import Enum
-from typing import Union
-from pydantic import BaseModel
+from typing import Union, List
+from pydantic import BaseModel, Field
 
 
 class Item(BaseModel):
     name: str
-    description: Union[str, None] = None
-    price: float
+    description: Union[str, None] = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(
+        gt=0, description="The price must be greater than zero"
+    )
     tax: Union[float, None] = None
+
+
+class User(BaseModel):
+    username: str
+    full_name: Union[str, None] = None
 
 
 class ModelName(str, Enum):
@@ -36,8 +45,40 @@ async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip: skip + limit]
 
 
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.get("/items2/")
+async def read_items(q: List[str] = Query(default=["foo", "bar"])):
+    query_items = {"q": q}
+    return query_items
+
+
+@app.get("/items3/")
+async def read_items(q: Union[str, None] = Query(
+    default=None,
+    alias="item-query",
+    title="Query string",
+    description="Query string for the items to search in the database that have a good match",
+    min_length=3,
+    max_length=50,
+    regex="^fixed-query$",
+    deprecated=True,
+    include_in_schema=False)
+):
+    query_items = {"q": q}
+    return query_items
+
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Union[str, None] = None, short:bool = False):
+async def read_item(item_id: str, q: Union[str, None] = Query(default=None, min_length=3, max_length=50,
+                                                              regex="^fixed-query$"), short: bool = False):
     item = {"item_id": item_id}
     if q:
         # return {"item_id": item_id, "q": q}
@@ -47,6 +88,64 @@ async def read_item(item_id: str, q: Union[str, None] = None, short:bool = False
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item = Body(embed=True), q: Union[str, None] = None):
+    # result = {"item_id": item_id, **item.dict()}
+    result = {"item_id": item_id, "item": item}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+@app.get("/items2/{item_id")
+async def read_items(
+        item_id: int = Path(title="The ID of the item go get"),
+        q: Union[str, None] = Query(default=None, alias="item-query"),
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.put("/items2/{item_id}")
+async def update_item(
+        *,
+        item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+        q: Union[str, None] = None,
+        item: Union[Item, None] = None):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results
+
+
+@app.get("/items3/{item_id")
+async def read_items(
+        *,
+        item_id: int = Path(title="The ID of the item go get",  gt=0, le=1000),
+        q: str,
+        size: float = Query(gt=0, lt=10.5)):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.put("/items3/{item_id}")
+async def update_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
+    return results
+
+
+@app.put("/items4/{item_id}")
+async def update_item(item_id: int, item: Item, user: User, importance: int = Body()):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
 
 
 @app.get("/models/{model_name}")
@@ -71,4 +170,4 @@ async def read_user_item(
         item.update(
             {"description": "This is an amazing item that has a long description"}
         )
-    return item;
+    return item
